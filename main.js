30bb7a7ce5c075ca8750216707d4c9d509d09a86
jenjordan4 <jbaldwin4@hotmail.com>
2020-04-14 19:19:36 -0400

    event handling

--- main.js
+++ main.js
@@ -4,6 +4,28 @@ var app = new Vue({
     product: 'Socks',
     description: 'These warm and wooly socks will keep your feet comfy!',
     image: './socks_green.jpg',
-    address: 'http://www.google.com'
+    inStock: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green',
+        variantImage: './socks_green.jpg'
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue',
+        variantImage: './socks_blue.jpg'
+      }
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart: function () {
+      this.cart += 1
+    },
+    updateProduct: function (variantImage) {
+      this.image = variantImage
+    }
   }
 })
